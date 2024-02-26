97003f072b8c28381fb21ca0eca84619bfab2400
LacoutierePaul <paul.lacoutiere@gmail.com>
2024-02-26 16:31:32 +0100

    create get routes

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,11 +1,15 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
+import { lastReceivedEncryptedMessage, lastReceivedDecryptedMessage, lastMessageDestination } from "./../users/user";
+
+
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
+
 
   // TODO implement the status route
   onionRouter.get("/status", (req, res) => {
@@ -13,6 +17,18 @@ export async function simpleOnionRouter(nodeId: number) {
       }
   );
 
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.status(200).json({result : lastReceivedEncryptedMessage})
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+        res.status(200).json({result : lastReceivedDecryptedMessage})
+
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+        res.status(200).json({result : lastMessageDestination})
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
