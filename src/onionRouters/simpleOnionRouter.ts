6953760c9a79bdc38b9af24db17ff6edf9ab3c91
LacoutierePaul <paul.lacoutiere@gmail.com>
2024-02-27 15:55:56 +0100

    Creating public and private key for the node and post them to the registry

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -2,7 +2,8 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 import { lastReceivedEncryptedMessage, lastReceivedDecryptedMessage, lastMessageDestination } from "./../users/user";
-
+import { generateRsaKeyPair, exportPubKey, exportPrvKey } from '../crypto';
+import axios from 'axios';
 
 
 export async function simpleOnionRouter(nodeId: number) {
@@ -10,10 +11,22 @@ export async function simpleOnionRouter(nodeId: number) {
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
+  // Generate a private and public key for the node
+  const { publicKey, privateKey } = await generateRsaKeyPair();
+  const pubKey = await exportPubKey(publicKey);
+
+
+
+  // Add a route to get the private key
+  onionRouter.get("/getPrivateKey", async (req, res) => {
+    const prvKey = await exportPrvKey(privateKey);
+    res.status(200).json({ result: prvKey });
+  });
+
 
   // TODO implement the status route
   onionRouter.get("/status", (req, res) => {
-    res.status(200).send("live");
+        res.status(200).send("live");
       }
   );
 
@@ -22,21 +35,30 @@ export async function simpleOnionRouter(nodeId: number) {
   });
 
   onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
-        res.status(200).json({result : lastReceivedDecryptedMessage})
+    res.status(200).json({result : lastReceivedDecryptedMessage})
 
   });
 
   onionRouter.get("/getLastMessageDestination", (req, res) => {
-        res.status(200).json({result : lastMessageDestination})
+    res.status(200).json({result : lastMessageDestination})
   });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
-      `Onion router ${nodeId} is listening on port ${
-        BASE_ONION_ROUTER_PORT + nodeId
-      }`
+        `Onion router ${nodeId} is listening on port ${
+            BASE_ONION_ROUTER_PORT + nodeId
+        }`
     );
   });
+
+    // Register the node in the registry
+    const registryUrl = `http://localhost:8080/registerNode`;
+    const registryBody = { nodeId, pubKey };
+    await axios.post(registryUrl, registryBody);
+
+
+
+
 
   return server;
 }
